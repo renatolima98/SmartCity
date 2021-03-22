0eb72eea343e8a6cddfddd85499a8efe6864a26b
renatolima98 <61155380+renatolima98@users.noreply.github.com>
2021-03-22 12:05:21 +0000

    criaçao da pagina notes e inicio do recycler

--- app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
+++ app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
@@ -1,12 +1,21 @@
 package ipvc.estg.smartcity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //teste git
+
+        val notesbutao = findViewById<Button>(R.id.notesBTN)
+
+        notesbutao.setOnClickListener {
+            val intent = Intent(this, Notes::class.java)
+            startActivity(intent)
+        }
     }
 }
+
